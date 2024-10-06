50e32c2a1a1db790f7b42912a24e9a0ccc9a040d
Ahmed <ahmed.el.azzabi@automattic.com>
2024-10-06 16:16:26 -0500

    Add script

---
+++ generate_distpackage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Print all files in the specified directories
+find includes -type f
+
+find build -type f
+
+find vendor -type f
+
+find assets -type f
+
+find build-extensions -type f
+
+echo "woocommerce-payments-subscriptions-migration.php"
